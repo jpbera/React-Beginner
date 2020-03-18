5f7082ade8d0d1b347e47ace62ad2b4d837ab51a
Jyoti Prakash Bera <jpbera@hotmail.com>
2020-03-18 17:45:45 +0530

    Commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+
+
+function HelloWorld() {
+    const [count, setCount] = useState(0)
+   const incrementCounter= () =>{
+    
+      setCount ( count=>count+1);
+    }
+    return (
+        <div>         
+          <h1>Hey There!!! I'm React Hook</h1>
+          <button onClick={()=>setCount ( count=>count+1)}>Count {count}</button>
+        </div>
+    )
+}
+
+export default HelloWorld
