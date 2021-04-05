1e5b3a978dc9a405b5e017ceff15d9945dcc0563
Jyoti Prakash Bera <jpbera@hotmail.com>
2021-04-05 18:59:35 +0530

    drbc Api

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
-app.listen(3000, () => console.log("server is running..."));
+const InitiateMongoServer = require("./app/config/dbconfig.js");
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to NodeAPI application." });
+});
+app.listen(process.env.LISTEN_PORT, () => console.log("server is running..." + process.env.LISTEN_PORT));
